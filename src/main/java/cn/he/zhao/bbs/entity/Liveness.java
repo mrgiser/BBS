d2907d4113f17e5b33285d86ded1c017466f6c09
hefeng <547669341@qq.com>
2018-08-28 18:44:30 +0800

    entity更新Liveness

--- src/main/java/cn/he/zhao/bbs/entity/Liveness.java
+++ src/main/java/cn/he/zhao/bbs/entity/Liveness.java
@@ -1,32 +1,6 @@
-/*
- * Symphony - A modern community (forum/BBS/SNS/blog) platform written in Java.
- * Copyright (C) 2012-2018, b3log.org & hacpai.com
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
 package cn.he.zhao.bbs.entity;
 
-import org.b3log.symphony.util.Symphonys;
-import org.json.JSONObject;
 
-/**
- * This class defines all liveness entity relevant keys.
- *
- * @author <a href="http://88250.b3log.org">Liang Ding</a>
- * @version 1.1.0.0, Jun 12, 2018
- * @since 1.4.0
- */
 public class Liveness {
 
     private String oid;
@@ -34,111 +8,151 @@ public class Liveness {
     /**
      * Key of user id.
      */
-    public static final String LIVENESS_USER_ID = "livenessUserId";
+    private String livenessUserId;
 
     /**
      * Key of liveness date.
      */
-    public static final String LIVENESS_DATE = "livenessDate";
+    private String livenessDate;
 
     /**
      * Key of liveness point.
      */
-    public static final String LIVENESS_POINT = "livenessPoint";
+    private Integer livenessPoint;
 
     /**
      * Key of liveness article.
      */
-    public static final String LIVENESS_ARTICLE = "livenessArticle";
+    private Integer livenessArticle;
 
     /**
      * Key of liveness comment.
      */
-    public static final String LIVENESS_COMMENT = "livenessComment";
+    private Integer livenessComment;
 
     /**
      * Key of liveness activity.
      */
-    public static final String LIVENESS_ACTIVITY = "livenessActivity";
+    private Integer livenessActivity;
 
     /**
      * Key of liveness thank.
      */
-    public static final String LIVENESS_THANK = "livenessThank";
+    private Integer livenessThank;
 
     /**
      * Key of liveness vote.
      */
-    public static final String LIVENESS_VOTE = "livenessVote";
+    private Integer livenessVote;
 
     /**
      * Key of liveness reward.
      */
-    public static final String LIVENESS_REWARD = "livenessReward";
+    private Integer livenessReward;
 
     /**
      * Key of liveness PV.
      */
-    public static final String LIVENESS_PV = "livenessPV";
+    private Integer livenessPV;
 
     /**
      * Key of liveness accept answer.
      */
-    public static final String LIVENESS_ACCEPT_ANSWER = "livenessAcceptAnswer";
+    private Integer livenessAcceptAnswer;
 
-    /**
-     * Calculates point of the specified liveness.
-     *
-     * @param liveness the specified liveness
-     * @return point
-     */
-    public static int calcPoint(final JSONObject liveness) {
-        final float activityPer = Symphonys.getFloat("activitYesterdayLivenessReward.activity.perPoint");
-        final float articlePer = Symphonys.getFloat("activitYesterdayLivenessReward.article.perPoint");
-        final float commentPer = Symphonys.getFloat("activitYesterdayLivenessReward.comment.perPoint");
-        final float pvPer = Symphonys.getFloat("activitYesterdayLivenessReward.pv.perPoint");
-        final float rewardPer = Symphonys.getFloat("activitYesterdayLivenessReward.reward.perPoint");
-        final float thankPer = Symphonys.getFloat("activitYesterdayLivenessReward.thank.perPoint");
-        final float votePer = Symphonys.getFloat("activitYesterdayLivenessReward.vote.perPoint");
-        final float acceptAnswerPer = Symphonys.getFloat("activitYesterdayLivenessReward.acceptAnswer.perPoint");
-
-        final int activity = liveness.optInt(Liveness.LIVENESS_ACTIVITY);
-        final int article = liveness.optInt(Liveness.LIVENESS_ARTICLE);
-        final int comment = liveness.optInt(Liveness.LIVENESS_COMMENT);
-        int pv = liveness.optInt(Liveness.LIVENESS_PV);
-        if (pv > 50) {
-            pv = 50;
-        }
-        final int reward = liveness.optInt(Liveness.LIVENESS_REWARD);
-        final int thank = liveness.optInt(Liveness.LIVENESS_THANK);
-        int vote = liveness.optInt(Liveness.LIVENESS_VOTE);
-        if (vote > 10) {
-            vote = 10;
-        }
-        final int acceptAnswer = liveness.optInt(Liveness.LIVENESS_ACCEPT_ANSWER);
-
-        final int activityPoint = (int) (activity * activityPer);
-        final int articlePoint = (int) (article * articlePer);
-        final int commentPoint = (int) (comment * commentPer);
-        final int pvPoint = (int) (pv * pvPer);
-        final int rewardPoint = (int) (reward * rewardPer);
-        final int thankPoint = (int) (thank * thankPer);
-        final int votePoint = (int) (vote * votePer);
-        final int acceptAnswerPoint = (int) (acceptAnswer * acceptAnswerPer);
-
-        int ret = activityPoint + articlePoint + commentPoint + pvPoint + rewardPoint + thankPoint + votePoint + acceptAnswerPoint;
-
-        final int max = Symphonys.getInt("activitYesterdayLivenessReward.maxPoint");
-        if (ret > max) {
-            ret = max;
-        }
-
-        return ret;
+    public String getOid() {
+        return oid;
     }
 
-    /**
-     * Private constructor.
-     */
-    private Liveness() {
+    public void setOid(String oid) {
+        this.oid = oid;
+    }
+
+    public String getLivenessUserId() {
+        return livenessUserId;
+    }
+
+    public void setLivenessUserId(String livenessUserId) {
+        this.livenessUserId = livenessUserId;
+    }
+
+    public String getLivenessDate() {
+        return livenessDate;
+    }
+
+    public void setLivenessDate(String livenessDate) {
+        this.livenessDate = livenessDate;
+    }
+
+    public Integer getLivenessPoint() {
+        return livenessPoint;
+    }
+
+    public void setLivenessPoint(Integer livenessPoint) {
+        this.livenessPoint = livenessPoint;
+    }
+
+    public Integer getLivenessArticle() {
+        return livenessArticle;
+    }
+
+    public void setLivenessArticle(Integer livenessArticle) {
+        this.livenessArticle = livenessArticle;
+    }
+
+    public Integer getLivenessComment() {
+        return livenessComment;
+    }
+
+    public void setLivenessComment(Integer livenessComment) {
+        this.livenessComment = livenessComment;
+    }
+
+    public Integer getLivenessActivity() {
+        return livenessActivity;
+    }
+
+    public void setLivenessActivity(Integer livenessActivity) {
+        this.livenessActivity = livenessActivity;
+    }
+
+    public Integer getLivenessThank() {
+        return livenessThank;
+    }
+
+    public void setLivenessThank(Integer livenessThank) {
+        this.livenessThank = livenessThank;
+    }
+
+    public Integer getLivenessVote() {
+        return livenessVote;
+    }
+
+    public void setLivenessVote(Integer livenessVote) {
+        this.livenessVote = livenessVote;
+    }
+
+    public Integer getLivenessReward() {
+        return livenessReward;
+    }
+
+    public void setLivenessReward(Integer livenessReward) {
+        this.livenessReward = livenessReward;
+    }
+
+    public Integer getLivenessPV() {
+        return livenessPV;
+    }
+
+    public void setLivenessPV(Integer livenessPV) {
+        this.livenessPV = livenessPV;
+    }
+
+    public Integer getLivenessAcceptAnswer() {
+        return livenessAcceptAnswer;
+    }
+
+    public void setLivenessAcceptAnswer(Integer livenessAcceptAnswer) {
+        this.livenessAcceptAnswer = livenessAcceptAnswer;
     }
 }
