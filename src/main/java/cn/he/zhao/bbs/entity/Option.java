2041a199a92d870a4fe28630528cd81f946ade42
hefeng <547669341@qq.com>
2018-08-28 18:48:39 +0800

    entity更新Option

--- src/main/java/cn/he/zhao/bbs/entity/Option.java
+++ src/main/java/cn/he/zhao/bbs/entity/Option.java
@@ -7,108 +7,34 @@ public class Option {
     /**
      * Key of option value.
      */
-    public static final String OPTION_VALUE = "optionValue";
+    private String optionValue;
 
     /**
      * Key of option category.
      */
-    public static final String OPTION_CATEGORY = "optionCategory";
+    private String optionCategory;
 
-    // oId constants
-    /**
-     * Key of member count.
-     */
-    public static final String ID_C_STATISTIC_MEMBER_COUNT = "statisticMemberCount";
+    public String getOid() {
+        return oid;
+    }
 
-    /**
-     * Key of article count.
-     */
-    public static final String ID_C_STATISTIC_ARTICLE_COUNT = "statisticArticleCount";
+    public void setOid(String oid) {
+        this.oid = oid;
+    }
 
-    /**
-     * Key of domain count.
-     */
-    public static final String ID_C_STATISTIC_DOMAIN_COUNT = "statisticDomainCount";
+    public String getOptionValue() {
+        return optionValue;
+    }
 
-    /**
-     * Key of tag count.
-     */
-    public static final String ID_C_STATISTIC_TAG_COUNT = "statisticTagCount";
+    public void setOptionValue(String optionValue) {
+        this.optionValue = optionValue;
+    }
 
-    /**
-     * Key of link count.
-     */
-    public static final String ID_C_STATISTIC_LINK_COUNT = "statisticLinkCount";
+    public String getOptionCategory() {
+        return optionCategory;
+    }
 
-    /**
-     * Key of comment count.
-     */
-    public static final String ID_C_STATISTIC_CMT_COUNT = "statisticCmtCount";
-
-    /**
-     * Key of max online visitor count.
-     */
-    public static final String ID_C_STATISTIC_MAX_ONLINE_VISITOR_COUNT = "statisticMaxOnlineVisitorCount";
-
-    /**
-     * Key of allow register.
-     */
-    public static final String ID_C_MISC_ALLOW_REGISTER = "miscAllowRegister";
-
-    /**
-     * Key of allow anonymous view.
-     */
-    public static final String ID_C_MISC_ALLOW_ANONYMOUS_VIEW = "miscAllowAnonymousView";
-
-    /**
-     * Key of allow add article.
-     */
-    public static final String ID_C_MISC_ALLOW_ADD_ARTICLE = "miscAllowAddArticle";
-
-    /**
-     * Key of allow add comment.
-     */
-    public static final String ID_C_MISC_ALLOW_ADD_COMMENT = "miscAllowAddComment";
-
-    /**
-     * Key of language.
-     */
-    public static final String ID_C_MISC_LANGUAGE = "miscLanguage";
-
-    /**
-     * Key of side full ad.
-     */
-    public static final String ID_C_SIDE_FULL_AD = "adSideFull";
-
-    /**
-     * Key of header banner.
-     */
-    public static final String ID_C_HEADER_BANNER = "headerBanner";
-
-    // Category constants
-    /**
-     * Statistic.
-     */
-    public static final String CATEGORY_C_STATISTIC = "statistic";
-
-    /**
-     * Miscellaneous.
-     */
-    public static final String CATEGORY_C_MISC = "misc";
-
-    /**
-     * Reserved words.
-     */
-    public static final String CATEGORY_C_RESERVED_WORDS = "reserved-words";
-
-    /**
-     * Ad.
-     */
-    public static final String CATEGORY_C_AD = "ad";
-
-    /**
-     * Private constructor.
-     */
-    private Option() {
+    public void setOptionCategory(String optionCategory) {
+        this.optionCategory = optionCategory;
     }
 }
