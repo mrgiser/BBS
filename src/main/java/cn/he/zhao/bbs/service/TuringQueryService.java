f7eb4c2ad95601aedebd48b423179076a0e52bba
hefeng <547669341@qq.com>
2018-09-15 16:46:27 +0800

    使用org.json

--- src/main/java/cn/he/zhao/bbs/service/TuringQueryService.java
+++ src/main/java/cn/he/zhao/bbs/service/TuringQueryService.java
@@ -3,9 +3,6 @@ package cn.he.zhao.bbs.service;
 import cn.he.zhao.bbs.service.interf.LangPropsService;
 import cn.he.zhao.bbs.util.Symphonys;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
 import org.apache.commons.lang.StringUtils;
 import org.apache.http.HttpEntity;
 import org.apache.http.StatusLine;
@@ -16,6 +13,8 @@ import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -116,10 +115,10 @@ public class TuringQueryService {
             StatusLine status = response.getStatusLine();
             int statusCode = status.getStatusCode();
             String responseStr = EntityUtils.toString(entity);
-            JSONObject json = JSON.parseObject(responseStr);
+            JSONObject json = new JSONObject(responseStr);
 
             final JSONObject intent = json.getJSONObject("intent");
-            final int code = intent.getInteger("code");
+            final int code = intent.optInt("code");
             final JSONArray results = json.getJSONArray("results");
             switch (code) {
                 case 5000:
@@ -149,7 +148,7 @@ public class TuringQueryService {
                 case 10008:
                 case 10011:
                     final StringBuilder builder = new StringBuilder();
-                    for (int i = 0; i < results.size(); i++) {
+                    for (int i = 0; i < results.length(); i++) {
                         final JSONObject result = results.getJSONObject(i);
                         final String resultType = result.getString("resultType");
                         String values = result.getJSONObject("values").getString(resultType);
