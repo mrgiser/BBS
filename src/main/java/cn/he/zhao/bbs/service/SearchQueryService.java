1a28eefd891f03aed354d464d77eef9cd0221787
hefeng <547669341@qq.com>
2018-09-28 11:38:08 +0800

    update SearchQueryService

--- src/main/java/cn/he/zhao/bbs/service/SearchQueryService.java
+++ src/main/java/cn/he/zhao/bbs/service/SearchQueryService.java
@@ -1,8 +1,19 @@
 package cn.he.zhao.bbs.service;
 
+import cn.he.zhao.bbs.entityUtil.ArticleUtil;
 import cn.he.zhao.bbs.util.Symphonys;
 import cn.he.zhao.bbs.entity.*;
 
+import cn.he.zhao.bbs.util.URLs;
+import org.apache.http.HttpEntity;
+import org.apache.http.StatusLine;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.slf4j.Logger;
@@ -35,7 +46,9 @@ public class SearchQueryService {
     /**
      * URL fetch service.
      */
-    private static final URLFetchService URL_FETCH_SVC = URLFetchServiceFactory.getURLFetchService();
+//    private static final URLFetchService URL_FETCH_SVC = URLFetchServiceFactory.getURLFetchService();
+    private static RequestConfig defaultRequestConfig = RequestConfig.custom().setConnectTimeout(3000)
+            .setConnectionRequestTimeout(1000).setSocketTimeout(10000).build();
 
     /**
      * Searches by Elasticsearch.
@@ -47,11 +60,16 @@ public class SearchQueryService {
      * @return search result, returns {@code null} if not found
      */
     public JSONObject searchElasticsearch(final String type, final String keyword, final int currentPage, final int pageSize) {
-        final HTTPRequest request = new HTTPRequest();
-        request.setRequestMethod(HTTPRequestMethod.POST);
+
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+
+        String url = SearchMgmtService.ES_SERVER + "/" + SearchMgmtService.ES_INDEX_NAME + "/" + type + "/_search";
         try {
-            request.setURL(new URL(SearchMgmtService.ES_SERVER + "/" + SearchMgmtService.ES_INDEX_NAME + "/" + type
-                    + "/_search"));
+            HttpPost request = new HttpPost(url);
+
+//        final HTTPRequest request = new HTTPRequest();
+//        request.setRequestMethod(HTTPRequestMethod.POST);
+//            request.setURL(new URL());
 
             final JSONObject reqData = new JSONObject();
             final JSONObject query = new JSONObject();
@@ -62,7 +80,7 @@ public class SearchQueryService {
             final JSONObject queryString = new JSONObject();
             must.put("query_string", queryString);
             queryString.put("query", "*" + keyword + "*");
-            queryString.put("fields", new String[]{Article.ARTICLE_TITLE, Article.ARTICLE_CONTENT});
+            queryString.put("fields", new String[]{ArticleUtil.ARTICLE_TITLE, ArticleUtil.ARTICLE_CONTENT});
             queryString.put("default_operator", "and");
             reqData.put("query", query);
             reqData.put("from", (currentPage - 1) * pageSize);
@@ -70,7 +88,7 @@ public class SearchQueryService {
             final JSONArray sort = new JSONArray();
             final JSONObject sortField = new JSONObject();
             sort.put(sortField);
-            sortField.put(Article.ARTICLE_CREATE_TIME, "desc");
+            sortField.put(ArticleUtil.ARTICLE_CREATE_TIME, "desc");
             sort.put("_score");
             reqData.put("sort", sort);
 
@@ -81,12 +99,25 @@ public class SearchQueryService {
             final JSONObject fields = new JSONObject();
             highlight.put("fields", fields);
             final JSONObject contentField = new JSONObject();
-            fields.put(Article.ARTICLE_CONTENT, contentField);
+            fields.put(ArticleUtil.ARTICLE_CONTENT, contentField);
             LOGGER.debug(reqData.toString(4));
-            request.setPayload(reqData.toString().getBytes("UTF-8"));
-            final HTTPResponse response = URL_FETCH_SVC.fetch(request);
+//            request.setPayload(reqData.toString().getBytes("UTF-8"));
 
-            return new JSONObject(new String(response.getContent(), "UTF-8"));
+            request.setConfig(defaultRequestConfig);
+
+            StringEntity strEntity = new StringEntity(reqData.toString(),"UTF-8");
+            request.setEntity(strEntity);
+
+            final CloseableHttpResponse response = httpClient.execute(request);
+            HttpEntity entity = response.getEntity();
+            StatusLine status = response.getStatusLine();
+            int statusCode = status.getStatusCode();
+            String responseStr = EntityUtils.toString(entity);
+            JSONObject json = new JSONObject(responseStr);
+
+//            final HTTPResponse response = URL_FETCH_SVC.fetch(request);
+
+            return json;
         } catch (final Exception e) {
             LOGGER.error( "Queries failed", e);
 
@@ -110,16 +141,22 @@ public class SearchQueryService {
         final String index = Symphonys.get("algolia.index");
         final String key = Symphonys.get("algolia.adminKey");
 
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+
         while (retries <= maxRetries) {
             String host = appId + "-" + retries + ".algolianet.com";
+            String url = "https://" + host + "/1/indexes/" + index + "/query";
 
             try {
-                final HTTPRequest request = new HTTPRequest();
-                request.addHeader(new HTTPHeader("X-Algolia-API-Key", key));
-                request.addHeader(new HTTPHeader("X-Algolia-Application-Id", appId));
+                HttpPost request = new HttpPost(url);
+                request.addHeader("X-Algolia-API-Key", key);
+                request.addHeader("X-Algolia-Application-Id", appId);
+//                final HTTPRequest request = new HTTPRequest();
+//                request.addHeader(new HTTPHeader("X-Algolia-API-Key", key));
+//                request.addHeader(new HTTPHeader("X-Algolia-Application-Id", appId));
 
-                request.setRequestMethod(HTTPRequestMethod.POST);
-                request.setURL(new URL("https://" + host + "/1/indexes/" + index + "/query"));
+//                request.setRequestMethod(HTTPRequestMethod.POST);
+//                request.setURL(new URL("https://" + host + "/1/indexes/" + index + "/query"));
 
                 final JSONObject params = new JSONObject();
                 params.put("params", "query=" + URLs.encode(keyword)
@@ -128,18 +165,29 @@ public class SearchQueryService {
                         + "&facetFilters=%5B%5D&maxValuesPerFacet=100"
                         + "&hitsPerPage=" + pageSize + "&page=" + (currentPage - 1));
 
-                request.setPayload(params.toString().getBytes("UTF-8"));
+//                request.setPayload(params.toString().getBytes("UTF-8"));
+                request.setConfig(defaultRequestConfig);
 
-                final HTTPResponse response = URL_FETCH_SVC.fetch(request);
+                StringEntity strEntity = new StringEntity(params.toString(),"UTF-8");
+                request.setEntity(strEntity);
 
-                final JSONObject ret = new JSONObject(new String(response.getContent(), "UTF-8"));
-                if (200 != response.getResponseCode()) {
-                    LOGGER.warn(ret.toString(4));
+//                final HTTPResponse response = URL_FETCH_SVC.fetch(request);
+                final CloseableHttpResponse response = httpClient.execute(request);
+                HttpEntity entity = response.getEntity();
+                StatusLine status = response.getStatusLine();
+                int statusCode = status.getStatusCode();
+                String responseStr = EntityUtils.toString(entity);
+                JSONObject json = new JSONObject(responseStr);
+
+
+//                final JSONObject ret = new JSONObject(new String(response.getContent(), "UTF-8"));
+                if (200 != statusCode) {
+                    LOGGER.warn(json.toString(4));
 
                     return null;
                 }
 
-                return ret;
+                return json;
             } catch (final UnknownHostException e) {
                 LOGGER.error( "Queries failed [UnknownHostException=" + host + "]");
 
